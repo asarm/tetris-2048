6f64fb7ea2ad594484da0d9803bafc0e888c4dd4
lilyjpeg <65497949+lilyjpeg@users.noreply.github.com>
2021-04-17 00:14:22 +0300

    New Tile moving algorithm. Sort of.

--- Tetris_2048.py
+++ Tetris_2048.py
@@ -22,7 +22,6 @@ class Game:
         stddraw.setXscale(-0.5, grid_w - 0.5)
         stddraw.setYscale(-0.5, grid_h - 0.5)
 
-
         # create the game grid
         grid = GameGrid(grid_h, grid_w)
 
@@ -68,7 +67,7 @@ class Game:
                 elif key_typed == "p":
                     print("Paused")
                     self.is_paused = not self.is_paused
-                    #self.display_game_menu(grid_h, grid_w)
+                    # self.display_game_menu(grid_h, grid_w)
 
                 # clear the queue that stores all the keys pressed/typed
                 stddraw.clearKeysTyped()
@@ -85,6 +84,25 @@ class Game:
                 self.game_over = grid.update_grid(tiles_to_place)
                 # end the main game loop if the game is over
 
+                merge = self.check_merging(grid)
+                while merge:
+                    merge = self.check_merging(grid)
+                labels, num_labels = self.connected_component_labeling(grid.tile_matrix, grid_w, grid_h)
+                print(labels)
+                print(num_labels)
+
+                copy_matrix = np.copy(grid.tile_matrix)
+
+                for x in range(grid_h):
+                    for y in range(grid_w):
+                        if grid.tile_matrix[x, y] != None and labels[x, y] != 1 \
+                                and grid.tile_matrix[x, y].get_position().x != 0:
+                            grid.tile_matrix[x, y - 1] = copy_matrix[x, y]
+                            grid.tile_matrix[x, y - 1].move(0, -1)
+                            copy_matrix = np.copy(grid.tile_matrix)
+                        labels, num_labels = self.connected_component_labeling(grid.tile_matrix, grid_w, grid_h)
+                        print("I'm here")
+
                 row_count = self.is_full(grid_h, grid_w, grid)
                 index = 0
 
@@ -93,30 +111,6 @@ class Game:
                         self.slide_down(row_count, grid)
                         row_count = self.is_full(grid_h, grid_w, grid)
                     index += 1
-
-                while True:
-                    if not self.check_merging(grid):
-                        for a in range(0, 19):
-                            for b in range(0, 12):
-                                if grid.tile_matrix[a][b] != None:
-                                    if b != 11 and b != 0:
-                                        if grid.tile_matrix[a + 1][b] == None and grid.tile_matrix[a - 1][b] == None and \
-                                                grid.tile_matrix[a][b + 1] == None and grid.tile_matrix[a][b - 1] == None:
-                                            print("Removed score:", grid.tile_matrix[a][b].number)
-                                            grid.tile_matrix[a][b].set_position(None)
-                                            grid.tile_matrix[a][b] = None
-                                    elif b == 11:
-                                        if grid.tile_matrix[a + 1][b] == None and grid.tile_matrix[a - 1][b] == None and grid.tile_matrix[a][b - 1] == None:
-                                            print("Removed score:", grid.tile_matrix[a][b].number)
-                                            grid.tile_matrix[a][b].set_position(None)
-                                            grid.tile_matrix[a][b] = None
-                                    elif b == 0:
-                                        if grid.tile_matrix[a + 1][b] == None and grid.tile_matrix[a - 1][b] == None and \
-                                                grid.tile_matrix[a][b + 1] == None:
-                                            print("Removed score:", grid.tile_matrix[a][b].number)
-                                            grid.tile_matrix[a][b].set_position(None)
-                                            grid.tile_matrix[a][b] = None
-                        break
 
                 if self.game_over:
                     print("Game Over")
@@ -283,6 +277,113 @@ class Game:
                     if mouse_x >= button_blc_x and mouse_x <= button_blc_x + button_w:
                         if mouse_y >= button_blc_y and mouse_y <= button_blc_y + button_h:
                             break  # break the loop to end the method and start the game
+
+    def connected_component_labeling(self, grid, grid_w, grid_h):
+        # initially all the pixels in the image are labeled as 0 (background)
+        labels = np.zeros([grid_h, grid_w], dtype=int)
+        # min_equivalent_labels list is used to store min equivalent label for each label
+        min_equivalent_labels = []
+        # labeling starts with 1 (as 0 is considered as the background of the image)
+        current_label = 1
+        # first pass to assign initial labels and determine minimum equivalent labels
+        # from conflicts for each pixel in the given binary image
+        # --------------------------------------------------------------------------------
+        for y in range(grid_h):
+            for x in range(grid_w):
+                if grid[y, x] is None:
+                    continue
+                # get the set of neighboring labels for this pixel
+                # using get_neighbor_labels function defined below
+                neighbor_labels = self.get_neighbor_labels(labels, (x, y))
+                # if all the neighbor pixels are background pixels
+                if len(neighbor_labels) == 0:
+                    # assign current_label as the label of this pixel
+                    # and increase current_label by 1
+                    labels[y, x] = current_label
+                    current_label += 1
+                    # initially the minimum equivalent label is the label itself
+                    min_equivalent_labels.append(labels[y, x])
+                # if there is at least one non-background neighbor
+                else:
+                    # assign minimum neighbor label as the label of this pixel
+                    labels[y, x] = min(neighbor_labels)
+                    # a conflict occurs if there are multiple (different) neighbor labels
+                    if len(neighbor_labels) > 1:
+                        labels_to_merge = set()
+                        # add min equivalent label for each neighbor to labels_to_merge set
+                        for l in neighbor_labels:
+                            labels_to_merge.add(min_equivalent_labels[l - 1])
+                        # update minimum equivalent labels related to this conflict
+                        # using update_equivalent_labels function defined below
+                        self.update_min_equivalent_labels(min_equivalent_labels, labels_to_merge)
+        # second pass to rearrange equivalent labels so they all have consecutive values
+        # starting from 1 and assign min equivalent label of each pixel as its own label
+        # --------------------------------------------------------------------------------
+        # rearrange min equivalent labels using rearrange_min_equivalent_labels function
+        self.rearrange_min_equivalent_labels(min_equivalent_labels)
+        # for each pixel in the given binary image
+        for y in range(grid_h):
+            for x in range(grid_w):
+                # get the value of the pixel
+                if grid[y, x] is None:
+                    continue
+                # assign minimum equivalent label of each pixel as its own label
+                labels[y, x] = min_equivalent_labels[labels[y, x] - 1]
+        # return the labels matrix and the number of different labels
+        return labels, len(set(min_equivalent_labels))
+
+    # Function for getting labels of the neighbors of a given pixel
+    def get_neighbor_labels(self, label_values, pixel_indices):
+        x, y = pixel_indices
+        # using a set to store different neighbor labels without any duplicates
+        neighbor_labels = set()
+        # add upper pixel to the set if the current pixel is not in the first row of the
+        # image and its value is not zero (not a background pixel)
+        if y != 0:
+            u = label_values[y - 1, x]
+            if u != 0:
+                neighbor_labels.add(u)
+        # add left pixel to the set if the current pixel is not in the first column of
+        # the image and its value is not zero (not a background pixel)
+        if x != 0:
+            l = label_values[y, x - 1]
+            if l != 0:
+                neighbor_labels.add(l)
+        # return the set of neighbor labels
+        return neighbor_labels
+
+    # Function for updating min equivalent labels by merging conflicting neighbor labels
+    # as the smallest value among their min equivalent labels
+    def update_min_equivalent_labels(self, all_min_eq_labels, min_eq_labels_to_merge):
+        # find the min value among conflicting neighbor labels
+        min_value = min(min_eq_labels_to_merge)
+        # for each minimum equivalent label
+        for index in range(len(all_min_eq_labels)):
+            # if its value is in min_eq_labels_to_merge
+            if all_min_eq_labels[index] in min_eq_labels_to_merge:
+                # update its value as the min_value
+                all_min_eq_labels[index] = min_value
+
+    # Function for rearranging min equivalent labels so they all have consecutive values
+    # starting from 1
+    def rearrange_min_equivalent_labels(self, min_equivalent_labels):
+        # find different values of min equivalent labels and sort them in increasing order
+        different_labels = set(min_equivalent_labels)
+        different_labels_sorted = sorted(different_labels)
+        # create an array for storing new (consecutive) values for min equivalent labels
+        new_labels = np.zeros(max(min_equivalent_labels) + 1, dtype=int)
+        count = 1  # first label value to assign
+        # for each different label value (sorted in increasing order)
+        for l in different_labels_sorted:
+            # determine the new label
+            new_labels[l] = count
+            count += 1  # increase count by 1 so that new label values are consecutive
+        # assign new values of each minimum equivalent label
+        for ind in range(len(min_equivalent_labels)):
+            old_label = min_equivalent_labels[ind]
+            new_label = new_labels[old_label]
+            min_equivalent_labels[ind] = new_label
+
 
 # start() function is specified as the entry point (main function) from which
 # the program starts execution
